5cce26c2c7fe136fea02bd8f08db92f41b4b1da4
Aryankamal <aryan.3264@gmail.com>
2018-02-15 12:09:18 +0100

    version 1.02

--- score_function.R
+++ score_function.R
@@ -15,14 +15,22 @@ cellnetGraph<-graph_from_data_frame(cellnet,directed=T)
 
 DElist1 = as.character(DElist1)
 
+
+germNet1 <- read.delim("germNet-FC1.txt", sep = "\t", header = T)
+germNet1 <- graph_from_data_frame(germNet1[,7:8],directed = T)
+g = germNet1
+newList  = read.delim("genelist1.txt" , header= F , stringsAsFactors = FALSE)
+geneList = newList$V1
+
+geneList = sample(V(g)$name , 273 )
 # g : igraph object, sub-network
 # geneList : a vector of biological process genes
 # iter: number of iteration for z-score
-Panir_subgraph <- function(g,geneList , iter = 1000 ) {
+Panir_subgraph <- function(g,geneList , iter = 1000 , lower_tail = TRUE) {
   
   if( is.igraph(g) == FALSE )
     stop("g should be igraph object")
-  if( is.character(geneList) )
+  if( is.character(geneList) == FALSE )
     stop("gene list shold be character")
   
   subGraph_geneNames <- intersect(geneList,V(g)$name)
@@ -39,10 +47,10 @@ Panir_subgraph <- function(g,geneList , iter = 1000 ) {
   m = intra_interaction / ( inter_interaction + intra_interaction)
   
   n = length(geneList)
-  vertexNames = V(g)
+  vertexNames = V(g)$name
   
   random_scores = lapply(1:iter, function(x){ 
-    tmp_names = sample(vertexNames , n )
+    tmp_names = sample(vertexNames , min(n,length(vertexNames)) )
     tmp_sub = induced_subgraph(g,tmp_names)
     
     intra_interaction_tmp = sum(degree(tmp_sub,mode="total" , loops = FALSE)) / 2
@@ -58,6 +66,25 @@ Panir_subgraph <- function(g,geneList , iter = 1000 ) {
   mean_scores = mean(random_scores)
   sd_scores = sd(random_scores)
   
-  z =  ( m- mean_scores) / sd_scores
-  print( 2*pnorm(-abs(z)))
+  z =  ( m - mean_scores) / sd_scores
+
+  pval =  pnorm(-abs(z) ,lower.tail = lower_tail)
+  
+  answer = list ( 
+    z_score = z,
+    modulation_score = m,
+    random_scores = random_scores,
+    pvalue = pval
+    )
+  class(answer) <- "Score_class"
+  setGeneric("Score_class", function(x) standardGeneric("Score_class"))
+
+  setMethod('summary', "Score_class",
+            function(object) {
+              cat("The mojulation score is : " , m , "\n")   
+              cat("The z_score is : " , z ,"\n")
+              cat("The pvalue is : " , pval , "\n")
+            }          
+  )
+  return(answer)
 }
